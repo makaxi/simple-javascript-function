16ecf3935b1ffc258b01594dcd501fe034e13cb8
makaxi <kamama919@gmail.com>
2022-12-31 23:20:08 -0800

    Add .js file to alert name, height, and country

---
+++ index.js
@@ -0,0 +1,9 @@
+function myFunction(name, height, country){
+  alert("Your name is " + name + ". You are " + height + " and live in " + country + ".");
+}
+
+myName = "James";
+myHeight = "5'4";
+myCountry = "USA";
+
+myFunction(myName, myHeight, myCountry);
